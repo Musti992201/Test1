057a6a9e79e481bb62fd6ffcaf8fdbd3e03e842b
Mustafa <Mustafa.Uyar@outlook.at>
2022-10-03 21:19:14 +0200

    Sub Method added

--- src/HelloDemo.java
+++ src/HelloDemo.java
@@ -1,13 +1,21 @@
+import java.time.chrono.MinguoChronology;
+
 public class HelloDemo {
 
     public static void main(String[] args) {
 
         System.out.println(Add(5,6));
+        System.out.println(Sub(5,6));
 
     }
 
     public static double Add(double a, int b){
         double Ergebnis=a+b;
         return Ergebnis;
+    }
+    public static double Sub(double a, double b){
+        double Ergebnis=a-b;
+        return  Ergebnis;
+
     }
 }
