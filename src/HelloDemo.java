84686b96b83674076e63a35f651508f3e384a862
Mustafa <Mustafa.Uyar@outlook.at>
2022-10-03 21:15:53 +0200

    Initial commit

---
+++ src/HelloDemo.java
@@ -0,0 +1,13 @@
+public class HelloDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println(Add(5,6));
+
+    }
+
+    public static double Add(double a, int b){
+        double Ergebnis=a+b;
+        return Ergebnis;
+    }
+}
